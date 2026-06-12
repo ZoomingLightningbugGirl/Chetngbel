754cb1384c138601cea069d2c79ee2a517cc2510
ZoomingLightningbugGirl <sophiepasquarella@gmail.com>
2026-06-12 14:03:30 -0400

    Game Updates

--- chetngbel/player_body.gd
+++ chetngbel/player_body.gd
@@ -4,7 +4,6 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -13,6 +12,14 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("MoveForward"):
+		velocity.x = 5
+	if Input.is_action_pressed("MoveBackward"):
+		velocity.x = -5
+	if Input.is_action_pressed("MoveRight"):
+		velocity.z = 5
+	if Input.is_action_pressed("MoveLeft"):
+		velocity.z = -5
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
